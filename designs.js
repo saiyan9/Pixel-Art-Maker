935576336f5f99d9e590fb0e3627b64052eb1f68
Sai <84168690+saiyan9@users.noreply.github.com>
2022-05-10 21:05:20 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,15 +1,8 @@
-// Select color input
-
-// Select size input
-
-
-
-
 // When size is submitted by the user, call makeGrid()
 $('#sizePicker').submit(function makeGrid(grid) {
-    $('table tr').remove();
-    var height = $('#inputHeight').val();
-    var width = $('#inputWidth').val();
+    $('table tr').remove();  // remove every previous table row
+    var height = $('#inputHeight').val(); //height of table or canvas
+    var width = $('#inputWidth').val(); //width of table or canvas
         for (var i=1; i<=height; i++) {
             $('table').append("<tr></tr>");
             for (var j=1; j<=width; j++) {
@@ -17,10 +10,11 @@ $('#sizePicker').submit(function makeGrid(grid) {
                 $('td').attr("class", 'cells');
             }
         }
-        grid.preventDefault();
-        $('.cells').click(function(event) {
+        grid.preventDefault(); 
+        $('.cells').click(function(event) {    //add color to cells when clicked
             var paint=$('#colorPicker').val();
             $(event.target).css('background-color', paint);
         });
     
 });
+
